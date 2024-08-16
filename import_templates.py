88a1a56aa4c98835664fc41c1a08a07bfa376ae0
Lauren Halstead Willett <59510099+laurenwillett@users.noreply.github.com>
2024-08-16 21:50:46 +0200

    Add files via upload

--- import_templates.py
+++ import_templates.py
@@ -149,7 +149,7 @@ def prep_scale_templates(def_wave_data, mean_resolution):
         template_SpTs.append(SpT)
         template_lums.append(L_template_log)
         template_names.append(template_name)
-        medians.append(np.median(dist_scaled_photosphere[int(np.where((def_wave_data-4500)==np.min(np.abs(def_wave_data-4500)))[0][0]):int(np.where((def_wave_data-5500)==np.min(np.abs(def_wave_data-5500)))[0][0])]))
+        medians.append(np.median(dist_scaled_photosphere[int(np.where(np.abs(def_wave_data-4500)==np.min(np.abs(def_wave_data-4500)))[0][0]):int(np.where(np.abs(def_wave_data-5500)==np.min(np.abs(def_wave_data-5500)))[0][0])]))
 
     template_Teffs = np.array(template_Teffs)
     template_lums = np.array(template_lums)
